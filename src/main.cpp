eab148f53180fe08f003ff9a30cd2d3f4f055fd7
Matthew O'Gorman <mog@rldn.net>
2016-02-06 20:33:09 -0500

    cant get it to take code...

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,17 @@
 #include "mbed.h"
 
 DigitalOut myled(P0_19);
+Serial pc(USBTX, USBRX); // tx, rx
 
 int main()
 {
-    while(1) {
-        myled = 1;
-        wait(1.2);
-        myled = 0;
-        wait(1.2);
-    }
+  myled = 1;
+  pc.baud(9600);
+  pc.printf("hello world\n");  
+  while(1) {
+    pc.printf("hello world\n"); 
+    wait(100);
+	//        myled = 0;
+	//        wait(10);
+  }
 }
