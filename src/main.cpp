105b68cfa031a34b4226b16fafc9f418c89e56e4
Matthew O'Gorman <mog@rldn.net>
2016-02-09 21:48:18 -0500

    got it working with softdevice linking

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,18 @@
 #include "mbed.h"
 
-DigitalOut myled(P0_19);
-Serial pc(USBTX, USBRX); // tx, rx
+DigitalOut myled(LED);
+//Serial pc(USBTX, USBRX); // tx, rx
 
 int main()
 {
-  myled = 1;
-  pc.baud(9600);
-  pc.printf("hello world\n");  
+  //  int myled;
+  //    pc.baud(9600);
+  //    pc.printf("hello world\n");  
   while(1) {
-    pc.printf("hello world\n"); 
-    wait(100);
-	//        myled = 0;
-	//        wait(10);
+    //    pc.printf("hello world\n");
+	myled = 1;
+wait(3);
+	myled = 0;
+	wait(3);
   }
 }
